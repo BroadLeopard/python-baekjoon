9b0b6b3e2a1cd3b3c87c24029961ca0bdf013975
BroadLeopard <whtngur155@gmail.com>
2022-03-14 00:30:27 +0900

    20915 품

--- 20915/main.py
+++ 20915/main.py
@@ -4,37 +4,18 @@ N = int(sys.stdin.readline())
 
 for i in range(N):
     t = sys.stdin.readline()
-    zero = t.count('0')
     t = t.replace('6', '9')
-    t = t.replace('0', '')
 
     li = list(t)
-    li.sort(reverse =True)
+    li.sort(reverse=True)
 
-    tmp = ['', '']
-    a = 1
-    b = 0
+    tmp = [li[0], li[1]]
 
-    for c in li:
-        if c == '\n':
-            continue
-
-        if a < 2:
+    for c in li[2:]:
+        if int(tmp[0] + c) * int(tmp[1]) > int(tmp[0]) * int(tmp[1] + c):
             tmp[0] += c
-            a += 1
-
-            if a == 2 and b == 2:
-                b = 0
-
-        elif b < 2:
+        else:
             tmp[1] += c
-            b += 1
 
-            if b == 2 and a == 2:
-                a = 0
-
-    for i in range(zero):
-        tmp[1] += '0'
-
-    print(tmp[0], tmp[1])
+    #print(tmp[0], tmp[1])
     print(int(tmp[0]) * int(tmp[1]))
