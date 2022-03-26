d4a55a465f5b4364974f01e1567619a3de97c1ef
BroadLeopard <whtngur155@gmail.com>
2022-03-26 17:43:34 +0900

    1 solved

--- 1987/main.py
+++ 1987/main.py
@@ -1,44 +1,34 @@
 import sys
 
 ans = 0
-
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
+path = [False] * 26
 
-
-
-
-def func(li, x, y, cnt):
+def func(x, y, cnt):#여태까지 넣은 알파벳, 그리고 다음에 이동할 위치 x y, 현재까지 이동한 길이
     global ans
 
-    if li[ord(keyboard[x][y]) - 65]:
-        ans = max(ans, cnt)
-    else:
-        for i in range(4):
-            if 0 <= x + dx[i] < R and 0 <= y + dy[i] < C and chk[x + dx[i]][y + dy[i]]:
-                chk[x][y] = False
-                li[ord(keyboard[x][y]) - 65] += 1
-                func(li, x + dx[i], y + dy[i], cnt + 1)
-                li[ord(keyboard[x][y]) - 65] -= 1
-                chk[x][y] = True
-            else:
-                ans = max(ans, cnt)
+    path[ord(keyboard[x][y]) - 65] = True
+
+    for i in range(4):
+        if 0 <= x + dx[i] < R and 0 <= y + dy[i] < C and not path[ord(keyboard[x + dx[i]][y + dy[i]]) - 65]:
+            # 다음에 올 알파벳은 지나간 적이 없는 경우 이때 알파벳이 경로도 포함한다.
+            func(x + dx[i], y + dy[i], cnt + 1)
+        else:
+            ans = max(ans, cnt)
+
+    path[ord(keyboard[x][y]) - 65] = False
+
 
 
 R, C = map(int, sys.stdin.readline().split())
 
 keyboard = []
-chk = []
 
 for _ in range(R):
     keyboard.append(list(sys.stdin.readline()[:-1]))
-    chk.append([True] * C)
 
-alpha = [0] * 26
 
-func(alpha, 0, 0, 0)#여태까지 넣은 알파벳, 그리고 다음에 이동할 위치, 길이
+func(0, 0, 1)
 
 print(ans)
-
-
-
