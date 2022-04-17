90806151d3401fbf5af64d546c2db11ae59d7756
BroadLeopard <whtngur155@gmail.com>
2022-04-17 16:48:53 +0900

    너무 어렵게 생각했다

--- 1790/main.py
+++ 1790/main.py
@@ -1,35 +1,27 @@
 import sys
 
 
-def func(n):#특정 수까지 길이를 구하는 함수
-    l = 0
-    for i in range(12):
-        if 10 ** (i + 1) <= n:
-            l += (i + 1) * 9 * 10 ** i
-        else:
-            l += (n - 10 ** i + 1) * (i + 1)
-            break
+N, k = map(int, sys.stdin.readline().split())
+num = 0# 어떤 숫자인지 가르킨다
+tmp = 0#임시 길이
+r = 0
 
-    return l
+for i in range(1, 10):
+    if tmp + 9*i*10**(i-1) <= k:
+        tmp += 9*i*10**(i-1)
+        num += 9*10**(i-1)
+    else:
+        r = (k - tmp) % i
+        if r: #r이 0이 아니면 내가 원하는 숫자는 q + 1
+            num += (k - tmp) // i + 1
+        else: #r이 0이면 그 숫자의 마지막 자리에 있다는 뜻
+            num += (k - tmp) // i
+        break
 
-
-N, K = map(int, sys.stdin.readline().split())
-tmp = 0
-start = 1
-end = N
-tmp = 0
-
-if func(N) < K:
+if N < num:
     print(-1)
 else:
-    while start <= end:
-        mid = (start + end) // 2
+    print(str(num)[r - 1])
 
-        if func(mid) < K:
-            start = mid + 1
-            tmp = start
-        else:
-            end = mid - 1
-            tmp = mid
 
-print(str(mid)[   ])
+
